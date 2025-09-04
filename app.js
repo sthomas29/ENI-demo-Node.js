a50dd05b55929d0ba9d268a8169ef279aa270fe2
Stéphane THOMAS <thomas91@free.fr>
2025-09-04 05:18:34 -0300

    Ajout d'un middleware pour vérifier l'exécution

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Ajout du middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} - ${req.get('User-Agent')}`)
+    next()
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log("Route par défaut");
+
+    res.send('Hello World !')
 })
 
 app.listen(port, () => {
