a35c05a1179e1e76991e48ae06f6d945d655663d
Stéphane THOMAS <thomas91@free.fr>
2025-09-04 05:30:36 -0300

    Ajout des routes cities/ et cities/:id

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const cities = ['Nantes', 'Paris', 'Quimper']
+
+
 // Ajout du middleware
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url} - ${req.get('User-Agent')}`)
@@ -15,6 +18,27 @@ app.get('/', (req, res) => {
 
     res.send('Hello World !')
 })
+
+
+// Route vers la liste de ville
+app.get('/cities', (req, res) => {
+    res.send(cities.join(', '))
+})
+
+// Route vers une ville spécifique selon son id
+app.get('/cities/:id', (req, res, next) => {
+    if (1 > req.params.id || req.params.id > cities.length) {
+        return res.status(404).send('Error: No city found')
+    }
+    res.send(cities[req.params.id - 1])
+})
+
+// Middleware captant l'erreur et affichant une page 404
+app.use((req, res) => {
+    res.status(404).send('Error 404: Page not found');
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
